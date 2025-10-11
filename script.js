7b77199505bdfe885b9728302509c1e67e0eff6f
Getahun Alemayhu <getahune.alemayhu@gmail.com>
2025-10-11 23:01:33 +0300

    add loaded function

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let toggled = document.querySelector("#toggle-units");
-let dropdown = document.querySelector(".dropdown-menu");
+Document.loaded = function () {
+  let toggled = document.querySelector("#toggle-units");
+  let dropdown = document.querySelector(".dropdown-menu");
 
-toggled.addEventListener("click", function (event) {
-  event.preventDefault();
-  if (dropdown.style.display == "none") {
-    dropdown.style.display = "block";
-    event.stopPropagation();
-    //Prevent body click from firing
-  } else {
-    dropdown.style.display == "block";
-  }
-});
+  toggled.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (dropdown.style.display == "none") {
+      dropdown.style.display = "block";
+      event.stopPropagation();
+      //Prevent body click from firing
+    } else {
+      dropdown.style.display == "block";
+    }
+  });
 
-document.body.addEventListener("click", function () {
-  dropdown.style.display = "none";
-});
+  document.body.addEventListener("click", function () {
+    dropdown.style.display = "none";
+  });
+};
